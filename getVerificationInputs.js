ed5094c5820e915bb26385cb550d39acf7c05ad1
jdwoj5butbetter <53838167+jdwoj5@users.noreply.github.com>
2020-11-30 16:48:42 -0500

    Update getVerificationInputs.js

--- getVerificationInputs.js
+++ getVerificationInputs.js
@@ -2,21 +2,23 @@
 var parser = require('cheerio')
 
 // Define
-exports.func = function(args) {
-	var $ = args.selector
-	if (!$) {
-		$ = parser.load(args.html)
+module.exports = {
+	func = (args) => {
+		var $ = args.selector
+		if (!$) {
+			$ = parser.load(args.html)
+		}
+		var inputs = {}
+		var find = [
+			'__VIEWSTATE',
+			'__VIEWSTATEGENERATOR',
+			'__EVENTVALIDATION',
+			'__RequestVerificationToken'
+		]
+		for (var i = 0; i < find.length; i++) {
+			var get = find[i]
+			inputs[get] = $('input[name=' + get + ']').val()
+		}
+		return inputs
 	}
-	var inputs = {}
-	var find = [
-		'__VIEWSTATE',
-		'__VIEWSTATEGENERATOR',
-		'__EVENTVALIDATION',
-		'__RequestVerificationToken'
-	]
-	for (var i = 0; i < find.length; i++) {
-		var get = find[i]
-		inputs[get] = $('input[name=' + get + ']').val()
-	}
-	return inputs
 }
